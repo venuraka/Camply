9da64ad306b6b5b68535ac8037e1029e72194a28
sachini konara <132426362+BrianFdo03@users.noreply.github.com>
2025-03-25 12:47:05 +0530

    Created Camp Details Display page and Create Camp Site Pages.
    Pages Done
    - Camp Details Display page
    - Create Camp Site
    
    Changed main to redirect directly to Camp Details Page
    
    Create Camp Site textboxes all working and output displayed in cmdline

---
+++ android/app/src/main/kotlin/com/example/camply/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.camply
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:location_details/main.dart';
+import 'package:camply/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
